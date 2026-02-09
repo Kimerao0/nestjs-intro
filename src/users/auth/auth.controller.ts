7d59b20ffbb00308d57224465ccee777bbede7f3
Alessandro Ceruti <alessando@fido.id>
2026-02-09 17:30:40 +0100

    Added global auth guard

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -1,24 +1,40 @@
-import { Body, ClassSerializerInterceptor, Controller, Post, SerializeOptions, UseInterceptors } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, NotFoundException, Post, SerializeOptions, UseInterceptors, Request } from '@nestjs/common';
+import { type AuthRequest } from 'src/users/auth/auth.request';
 import { AuthResponse } from 'src/users/auth/auth.response';
 import { AuthService } from 'src/users/auth/auth.service';
 import { CreateUserDto } from 'src/users/create-user.dto';
 import { LoginDto } from 'src/users/login.dto';
 import { LoginResponse } from 'src/users/login.response';
+import { User } from 'src/users/user.entity';
+import { UserService } from 'src/users/user/user.service';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
 @SerializeOptions({ strategy: 'excludeAll' })
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @Post('register')
-  async register(@Body() createUserDto: CreateUserDto): Promise<AuthResponse> {
+  public async register(@Body() createUserDto: CreateUserDto): Promise<AuthResponse> {
     return await this.authService.register(createUserDto);
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto): Promise<LoginResponse> {
+  public async login(@Body() loginDto: LoginDto): Promise<LoginResponse> {
     const accessToken = await this.authService.login(loginDto.email, loginDto.password);
     return new LoginResponse({ accessToken });
+  }
+
+  @Post('/profile')
+  public async profile(@Request() req: AuthRequest): Promise<User> {
+    const user = await this.userService.findOneById(req.user.sub);
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -10,6 +10,7 @@ import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
 import { PasswordService } from 'src/users/password/password.service';
 import { AuthGuard } from 'src/users/auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -26,7 +27,16 @@ import { AuthGuard } from 'src/users/auth.guard';
         }) as JwtModuleOptions,
     }),
   ],
-  providers: [UserService, AuthService, PasswordService, AuthGuard],
+  providers: [
+    UserService,
+    AuthService,
+    PasswordService,
+    AuthGuard,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
 })
 export class UsersModule {}
